62d76475ae65219baa3498f50eeb048b58e2bf8e
Rodrigo de la Parra <rodrigo.delaparra@gmail.com>
2021-02-01 17:53:11 -0500

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		int x=0;
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
